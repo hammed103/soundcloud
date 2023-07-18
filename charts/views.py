b1a8fbf9dcf563038073bba4b19a183707dbb231
hammed103 <hammedfree@gmail.com>
2023-07-18 16:11:04 +0100

    link

--- charts/views.py
+++ charts/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from .models import Chart
 import requests
 
@@ -13,104 +17,107 @@ def top_50(request):
 
 
 
+class refreshMain(APIView):
+    @staticmethod
+    def generate_top_50(req):
 
-def generate_top_50(current_chart):
-    # Get the previous top 50 chart
-    previous_chart = Chart.objects.order_by('current_position')
+        headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Origin': 'https://soundcloud.com',
+        'Referer': 'https://soundcloud.com/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320 Edg/114.0.0.0',
+    }
 
-    # Create a dictionary to store the song positions
-    song_positions = {}
+        response = requests.get(
+            'https://api-v2.soundcloud.com/search/tracks?q=*&filter.genre_or_tag=hardstyle&sort=popular&client_id=w2Cs8NzMrJqhjiCIinZ1xxNBqPNgTVIe&limit=50&offset=0&linked_partitioning=1&app_version=1689322736&app_locale=en',
+            headers=headers,
+        )
+        dt = response.json()
+        current_chart = [{"current_position":index+1,"title":i["title"],"link":i["permalink_url"]} for index,i in enumerate(dt["collection"])][:50]
+        # Get the previous top 50 chart
+        previous_chart = Chart.objects.order_by('current_position')
 
-    # Populate the song_positions dictionary with previous positions
-    for i, song in enumerate(previous_chart, start=1):
-        song_positions[song.title] = (song.current_position, None)
+        # Create a dictionary to store the song positions
+        song_positions = {}
 
-
-    # Update the song_positions dictionary with current positions and update database
-    for i, song in enumerate(current_chart, start=1):
-        song_title = song['title']
-        curr_pos = song["current_position"]
-        print(curr_pos)
-        link = song["link"]
-        if song_title in song_positions:
-            prev_pos, _ = song_positions[song_title]
-            print(prev_pos)
-            chart_obj = Chart.objects.get(title=song_title)
-            chart_obj.previous_position = prev_pos
-            chart_obj.current_position = curr_pos
-            chart_obj.indicator = prev_pos - curr_pos
-            print(prev_pos - curr_pos)
-            chart_obj.link = link
-            chart_obj.save()
-        else:
-            import spotipy
-            from spotipy.oauth2 import SpotifyClientCredentials
-
-            import re
-
-            def remove_bracket_content(input_string):
-                pattern = r"\([^()]*\)"
-                return re.sub(pattern, "", input_string)
+        # Populate the song_positions dictionary with previous positions
+        for i, song in enumerate(previous_chart, start=1):
+            song_positions[song.title] = (song.current_position, None)
 
 
-            # Set your client key and secret
-            client_id = "53fb1dbe5f42480ba654fcc3c7e168d6"
-            client_secret = "5c1da4cce90f410e88966cdfc0785e3a"
-
-            # Initialize the Spotify client
-            client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-            sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-            # Example: Search for a track and retrieve its information
-            track_name = song_title
-            track_name = remove_bracket_content(track_name)
-            results = sp.search(q=track_name, type="track", limit=1)
-
-            if results["tracks"]["total"] > 0:
-                track = results["tracks"]["items"][0]
-                spot_name = track["name"]
-                spot_url = track["href"]
-
+        # Update the song_positions dictionary with current positions and update database
+        for i, song in enumerate(current_chart, start=1):
+            song_title = song['title']
+            curr_pos = song["current_position"]
+            print(curr_pos)
+            link = song["link"]
+            if song_title in song_positions:
+                prev_pos, _ = song_positions[song_title]
+                print(prev_pos)
+                chart_obj = Chart.objects.get(title=song_title)
+                chart_obj.previous_position = prev_pos
+                chart_obj.current_position = curr_pos
+                chart_obj.indicator = prev_pos - curr_pos
+                print(prev_pos - curr_pos)
+                chart_obj.link = link
+                chart_obj.save()
             else:
-                spot_name = None
-                spot_url = None
+                import spotipy
+                from spotipy.oauth2 import SpotifyClientCredentials
 
-                        # Example: Search for a track and retrieve its information
-            track_name = song_title + " hardstyle"
-            results = sp.search(q=track_name, type="track", limit=1)
+                import re
 
-            if results["tracks"]["total"] > 0:
-                track = results["tracks"]["items"][0]
-                comp_name= track["name"]
-                comp_url = track["href"]
-
-            else:
-                comp_name = None
-                comp_url = None
-                
-            # Add the new song to the database
-            chart_obj = Chart(title=song_title, previous_position=None, current_position= curr_pos,indicator= None ,link = link ,spot_name= spot_name, spot_url = spot_url,comp_name=comp_name,comp_url=comp_url)
-            chart_obj.save()
-    
+                def remove_bracket_content(input_string):
+                    pattern = r"\([^()]*\)"
+                    return re.sub(pattern, "", input_string)
 
 
-headers = {
-    'Accept': 'application/json, text/javascript, */*; q=0.01',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Connection': 'keep-alive',
-    'Origin': 'https://soundcloud.com',
-    'Referer': 'https://soundcloud.com/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-site',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320 Edg/114.0.0.0',
-}
+                # Set your client key and secret
+                client_id = "53fb1dbe5f42480ba654fcc3c7e168d6"
+                client_secret = "5c1da4cce90f410e88966cdfc0785e3a"
 
-response = requests.get(
-    'https://api-v2.soundcloud.com/search/tracks?q=*&filter.genre_or_tag=hardstyle&sort=popular&client_id=w2Cs8NzMrJqhjiCIinZ1xxNBqPNgTVIe&limit=50&offset=0&linked_partitioning=1&app_version=1689322736&app_locale=en',
-    headers=headers,
-)
-dt = response.json()
-current_chart = [{"current_position":index+1,"title":i["title"],"link":i["permalink_url"]} for index,i in enumerate(dt["collection"])][:50]
+                # Initialize the Spotify client
+                client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+                sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+                # Example: Search for a track and retrieve its information
+                track_name = song_title
+                track_name = remove_bracket_content(track_name)
+                results = sp.search(q=track_name, type="track", limit=1)
+
+                if results["tracks"]["total"] > 0:
+                    track = results["tracks"]["items"][0]
+                    spot_name = track["name"]
+                    spot_url = track["href"]
+
+                else:
+                    spot_name = None
+                    spot_url = None
+
+                            # Example: Search for a track and retrieve its information
+                track_name = song_title + " hardstyle"
+                results = sp.search(q=track_name, type="track", limit=1)
+
+                if results["tracks"]["total"] > 0:
+                    track = results["tracks"]["items"][0]
+                    comp_name= track["name"]
+                    comp_url = track["href"]
+
+                else:
+                    comp_name = None
+                    comp_url = None
+                    
+                # Add the new song to the database
+                chart_obj = Chart(title=song_title, previous_position=None, current_position= curr_pos,indicator= None ,link = link ,spot_name= spot_name, spot_url = spot_url,comp_name=comp_name,comp_url=comp_url)
+                chart_obj.save()
+        
+
+
+
 
 generate_top_50(current_chart)
