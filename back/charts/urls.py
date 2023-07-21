11015130015300d4b03ee42198f47def2bdb667a
hammed103 <hammedfree@gmail.com>
2023-07-21 19:30:42 +0100

    tiktok

--- back/charts/urls.py
+++ back/charts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from charts.views import Update,Render,tik,tiktok_view
+from charts.views import Update, Render, tik, tiktok_view
 
 
 urlpatterns = [
-    path('/api/tiktok', tiktok_view, name='tiktok'),
-    path("api/update",Update.as_view()),
-    path("api/render",Render.as_view()),
-     path("api/tik",tik.as_view())
+    path("api/tiktok", tiktok_view, name="tiktok"),
+    path("api/update", Update.as_view()),
+    path("api/render", Render.as_view()),
+    path("api/tik", tik.as_view()),
 ]
