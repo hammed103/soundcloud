dd548c7716e7ec1b3e83341d1045b4dc0627259a
hammed103 <hammedfree@gmail.com>
2023-07-21 16:03:38 +0000

    live

--- back/charts/urls.py
+++ back/charts/urls.py
@@ -4,7 +4,6 @@ from charts.views import Update,Render
 
 
 urlpatterns = [
-    path('', views.top_50, name='top_50'),
     path("api/update",Update.as_view()),
     path("api/render",Render.as_view())
 ]
