cba4419be91700dc87f8f84c3ac3f3defdb09081
hammed103 <hammedfree@gmail.com>
2023-07-23 19:36:21 +0100

    tiktok

--- back/top50chart/settings.py
+++ back/top50chart/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = "top50chart.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'soundcloud',
+        'USER': 'postgres',
+        'PASSWORD': '1111',
+        'HOST': '35.202.13.40',  # Typically 'localhost' if running locally
+    }
+}
 
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
+}"""
 
 
 # Password validation
