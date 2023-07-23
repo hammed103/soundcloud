a497694270a37a234cdd6bd203118a2d3c9e5463
hammed103 <hammedfree@gmail.com>
2023-07-23 18:20:34 +0100

    tiktok

--- back/charts/models.py
+++ back/charts/models.py
@@ -1,20 +1,22 @@
 from django.db import models
 
+
 class Chart(models.Model):
-    tags = models.CharField(max_length=100,null=True)
-    title = models.CharField(max_length=100,)
+    tags = models.CharField(max_length=100, null=True)
+    title = models.CharField(
+        max_length=100,
+    )
     previous_position = models.IntegerField(null=True)
-    current_position = models.IntegerField()
+    current_position = models.IntegerField(null=True)
     link = models.URLField()
     indicator = models.IntegerField(null=True)
-    spot_name = models.CharField(max_length=100,null=True)
+    spot_name = models.CharField(max_length=100, null=True)
     spot_url = models.URLField(null=True)
-    comp_name = models.CharField(max_length=100,null=True)
-    comp_artist = models.CharField(max_length=100,null=True)
+    comp_name = models.CharField(max_length=100, null=True)
+    comp_artist = models.CharField(max_length=100, null=True)
     comp_url = models.URLField(null=True)
     sound_likes = models.IntegerField(null=True)
     sound_play = models.IntegerField(null=True)
     sound_repost = models.IntegerField(null=True)
     sound_release = models.DateTimeField()
-    lastweek =  models.IntegerField(null=True)
-
+    lastweek = models.IntegerField(null=True)
