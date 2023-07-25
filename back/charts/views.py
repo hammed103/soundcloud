9b03f8953622598db9abdc838bbc7d85b040a02b
hammed103 <hammedfree@gmail.com>
2023-07-25 14:06:44 +0100

    change settings

--- back/charts/views.py
+++ back/charts/views.py
@@ -22,27 +22,92 @@ table_name = "Tiktok"
 
 airtable = pyairtable.Table(api_key, base_id, table_name)
 
-"""from django.db.models import Count
 
-# Step 1: Identify the duplicate entries (based on song title and tags)
-duplicate_songs = (
-    Chart.objects.values("title", "tags")
-    .annotate(count=Count("id"))
-    .filter(count__gt=1)
-)
+music_types = ['electronic', 'all-music', 'house', 'world', 'electronic', 'pop', 'rock', 'danceedm', 'techno', 'rbsoul', 'deephouse', 'ambient', 'soundtrack', 'drumbass', 'trance', 'country', 'alternativerock', 'indie', 'piano']
+import requests
 
-# Step 2: Decide which entry to keep (let's assume we keep the one with the highest ID)
-for entry in duplicate_songs:
-    song_title = entry["title"]
-    song_tags = entry["tags"]
-    # Get the duplicate entries with the same title and tags
-    duplicate_entries = Chart.objects.filter(title=song_title, tags=song_tags)
+headers = {
+    'Accept': 'application/json, text/javascript, */*; q=0.1',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Authorization': 'OAuth 2-294078-444389085-iIl21X0gbFjMWd',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/json',
+    'Origin': 'https://soundcloud.com',
+    'Referer': 'https://soundcloud.com/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320 Edg/115.0.0.0',
+}
 
-    # Decide which entry to keep (e.g., the one with the highest ID)
-    entry_to_keep = duplicate_entries.order_by("-id").first()
+params = {
+    'ids': '1056989302,1051523650,111127967,115236819,1194533935,1204457869,1242757588,1247116825,1263196699,1267447333,1269345835,1273484212,1275853348,1301428681,1339623658,1389272428,1393753888,1418341354,1428974836,1436403271,1436403670,1441741387,1443741343,1449523786,1454585971,1459277209,1459277581,1459277827,1459278433,1459278556,1459278877,1459279579,1460303899,1485545800,1491677641,1520926177,180905489,247837953,253006715,383244017,646736838,665261066,673254992,709649923,887243206,887244826,894055741,930408532,959334589,959336380',
+    'client_id': 'MK6Otkm10RJQcH3Cju78UhH6NXw40V47',
+    '[object Object]': '',
+    'app_version': '1690193099',
+    'app_locale': 'en',
+}
+countries_tuple = [    ("Germany", "DE"),
+    ("United Kingdom", "GB"),
+    ("United States", "US"),
+    ("Netherlands", "NL"),
+    ("France", "FR"),
+    ("Australia", "AU"),
+    ("Brazil", "BR"),
+    ("Poland", "PL"),
+    ("Sweden", "SE"),
+    ("Austria", "AT"),
+    ("India", "IN"),
+    ("Canada", "CA"),
+    ("Turkey", "TR"),
+    ("Switzerland", "CH"),
+    ("Norway", "NO"),
+    ("Indonesia", "ID"),
+    ("Mexico", "MX"),
+    ("New Zealand", "NZ"),
+    ("Belgium", "BE"),
+    ("Ireland", "IE"),
+    ("Italy", "IT"),
+    ("Portugal", "PT"),
+    ("Spain", "ES"),
+    ("Denmark", "DK"),
+    ("Finland", "FI")
+]
 
-    # Step 3: Delete the duplicate entries (excluding the one we decided to keep)
-    duplicate_entries.exclude(id=entry_to_keep.id).delete()"""
+import requests
+
+headers = {
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320 Edg/115.0.0.0',
+}
+
+from bs4 import BeautifulSoup
+
+
+def create_soup_from_html(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        html_content = response.text
+        soup = BeautifulSoup(html_content, "html.parser")
+        return soup
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching the URL: {e}")
+        return None
+
+import json
+def extract_dictionary_from_html(url):
+    soup = create_soup_from_html(url)
+    script_tags = soup.find_all("script")
+    
+    for script in script_tags:
+        if "__sc_hydration" in str(script):
+            data_start = str(script).find("[{")
+            data_end = str(script).rfind("}]") + 2
+            json_data = str(script)[data_start:data_end]
+            data_dict = json.loads(json_data)
+            return data_dict
+
 
 
 @csrf_exempt
@@ -280,7 +345,7 @@ def generate_top_50(current_chart, today):
                     today=today,
                 )
                 chart_obj.save()
-                
+
             except:
                 continue
             # Set position_7_days_ago for the new entry
@@ -528,6 +593,7 @@ class Update(APIView):
             "remix",
             "viral",
             "hardtechno",
+            "rap techno"
         ]:
             response = requests.get(
                 f"https://api-v2.soundcloud.com/search/tracks?q=*&filter.genre_or_tag={tag}&sort=popular&client_id=w2Cs8NzMrJqhjiCIinZ1xxNBqPNgTVIe&limit=50&offset=0&linked_partitioning=1&app_version=1689322736&app_locale=en",
@@ -564,46 +630,47 @@ class Update(APIView):
 class Discover(APIView):
     @staticmethod
     def get(req):
-        headers = {
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Connection": "keep-alive",
-            "Origin": "https://soundcloud.com",
-            "Referer": "https://soundcloud.com/",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-site",
-            "User-Agent": "Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.188 Safari/537.36 CrKey/1.54.250320 Edg/114.0.0.0",
-        }
-        current_charts = []
-        params = {
-            "ids": "1051523650,1056989302,111127967,115236819,1194533935,1204457869,1242757588,1247116825,1263196699,1267447333,1269345835,1273484212,1275853348,1301428681,1339623658,1389272428,1393753888,1418341354,1428974836,1436403271,1436403670,1441741387,1443741343,1449523786,1454585971,1459277209,1459277581,1459277827,1459278433,1459278556,1459278877,1459279579,1460303899,1485545800,1491677641,1520926177,180905489,247837953,253006715,255790653,383244017,646736838,665261066,673254992,709649923,887243206,894055741,930408532,959334589,959336380",
-            "client_id": "MK6Otkm10RJQcH3Cju78UhH6NXw40V47",
-            "[object Object]": "",
-            "app_version": "1690193099",
-            "app_locale": "en",
-        }
+        for country,co in countries_tuple : 
+            for typex in music_types :
+                url = f'https://soundcloud.com/discover/sets/charts-top:{typex}:{co}'
+                data = extract_dictionary_from_html(url)
+                dummy = [str(i["id"]) for i in data[6]["data"]["tracks"]]
 
-        response = requests.get(
-            "https://api-v2.soundcloud.com/tracks", params=params, headers=headers
-        )
-        dt = response.json()
-        current_chart = [
-            {
-                "tags": "top-charts",
-                "country": "us",
-                "current_position": index + 1,
-                "title": i["title"],
-                "link": i["permalink_url"],
-                "sound_likes": i["likes_count"],
-                "sound_play": i["playback_count"],
-                "sound_repost": i["reposts_count"],
-                "sound_release": i["display_date"],
-            }
-            for index, i in enumerate(response.json())
-        ]
+                # Sort the new_ids_list alphabetically
+                dummy.sort()
 
-        generate_discover(current_chart, today=today)
+                # Convert the sorted list back to a string with comma separation
+                new_ids_str = ','.join(dummy)
+
+                # Format the params dictionary with the new sorted ids
+                params_formatted = params.copy()
+                params_formatted['ids'] = new_ids_str
+
+
+
+
+                response = requests.get(
+                    "https://api-v2.soundcloud.com/tracks", params=params_formatted, headers=headers
+                )
+                response.json()
+                dt = response.json()
+                current_chart = [
+                    {
+                        "tags": f"{typex}",
+                        "country": f"{country}",
+                        "current_position": index + 1,
+                        "title": i["title"],
+                        "link": i["permalink_url"],
+                        "sound_likes": i["likes_count"],
+                        "sound_play": i["playback_count"],
+                        "sound_repost": i["reposts_count"],
+                        "sound_release": i["display_date"],
+                    }
+                    for index, i in enumerate(response.json())
+                ]
+                
+
+                generate_discover(current_chart, today=today)
 
         return Response(
             {
