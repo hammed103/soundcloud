90db86f985605fff00988246d9911672f42ec6fc
hammed103 <hammedfree@gmail.com>
2023-07-27 12:38:44 +0100

    on n

--- back/charts/views.py
+++ back/charts/views.py
@@ -23,7 +23,7 @@ table_name = "Tiktok"
 
 from datetime import date, timedelta
 
-today = date.today() - timedelta(days=1)
+today = date.today() 
 
 airtable = pyairtable.Table(api_key, base_id, table_name)
 
